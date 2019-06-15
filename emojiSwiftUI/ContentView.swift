d82378ec635f8a5259a82863b9b646a4c897e554
Dr. Andreas Plagens <andreas@plagens.eu>
2019-06-15 22:01:54 +0200

    Initial Commit

---
+++ emojiSwiftUI/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  emojiSwiftUI
+//
+//  Created by Dr. Andreas Plagens on 15.06.19.
+//  Copyright © 2019 Dr. Andreas Plagens. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
